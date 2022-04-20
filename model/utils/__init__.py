deacd4d3b2a61eb3b30998ff116acf611431d52f
qixitan <87851193+qixitan@users.noreply.github.com>
2022-04-20 15:50:04 +0800

    Add files via upload

---
+++ model/utils/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+# @Author: qixitan
+# @Email: qixitan@qq.com
+# @FileName: __init__.py
+# @Time: 2022/4/18 21:49
+
+from .allreduce_norm import *
+from .dist import *
+from .logger import setup_logger
+from .model_utils import *
+from .checkpoint import *
+from .metric import *
+from .ema import *
---
+++ model/utils/transform.py
@@ -0,0 +1,33 @@
+# !/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+# @Author: qixitan
+# @Email: qixitan@qq.com
+# @FileName: transformer.py
+# @Time: 2022/3/1 16:18
+import torchvision.transforms as transforms
+
+cifar_transform = {
+    "train":
+        transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.4914, 0.4882, 0.4465],
+                std=[0.2023, 0.1994, 0.2010],
+            )
+        ]),
+    "val":
+        transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.4914, 0.4882, 0.4465],
+                std=[0.2023, 0.1994, 0.2010],
+            )
+        ])
+}
+
+
+# Imagenet
+# mean,val=[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+
